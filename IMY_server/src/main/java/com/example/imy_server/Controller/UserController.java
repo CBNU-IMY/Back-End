74f8ff166121a0920a9bc49b7c31e25454e0441f
JAY-HELLO <double04j@gmail.com>
2022-10-12 13:12:39 +0900

    update Query delete&update

--- IMY_server/src/main/java/com/example/imy_server/Controller/UserController.java
+++ IMY_server/src/main/java/com/example/imy_server/Controller/UserController.java
@@ -45,17 +45,21 @@ public class UserController {
         return userOptional;
     }
     //회원삭제
-    @DeleteMapping("/user")
-    public void delete(@RequestParam Long user_id) {
-        userRepository.deleteById(user_id);
+    //user id를 받았을때, user pk를 반환받아서 처리해야함.
+    //user_id가 여러개일때 오류가 발생함으로 test시 user_id가 중복되지 않도록 할 것.
+    @DeleteMapping("/user/{user_id}")
+    public void delete(@PathVariable String user_id) {
+        Long user_pk = userRepository.findPkByUserId(user_id);
+        userRepository.deleteById(user_pk);
     }
 
     //회원 수정
     //id로 회원정보 수정정
     @PutMapping("/user/{user_id}")
-    public User update(@PathVariable Long user_id, @RequestParam String user_pw, @RequestParam String user_name,
+    public User update(@PathVariable String user_id, @RequestParam String user_pw, @RequestParam String user_name,
                        @RequestParam String user_phone) {
-        Optional<User> user = userRepository.findById(user_id);
+        Long user_pk = userRepository.findPkByUserId(user_id);
+        Optional<User> user = userRepository.findById(user_pk);
 
         //아이디를 바꾸어서는 안되니 id와 권한을 제외한 정보 변경
         User _user = user.get();
