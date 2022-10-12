68229671b24977c92a3eb2a5b5f0a527b7c16840
the-groot <sangwoo0795@naver.com>
2022-10-12 20:38:27 +0900

    user테이블 이름 변경

--- IMY_server/src/main/java/com/example/imy_server/Domain/User.java
+++ IMY_server/src/main/java/com/example/imy_server/Domain/User.java
@@ -3,20 +3,31 @@ package com.example.imy_server.Domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@Table(name="users")
 public class User {
 
     @Id @GeneratedValue
-    private Long user_pk;
-    private String user_id;
-    private String user_pw;
-    private String user_name;
-    private String user_phone;
-    private String user_auth;
+
+    @Column(name = "user_pk")
+    private Long userPk;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "user_pw")
+    private String userPw;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_phone")
+    private String userPhone;
+
+    @Column(name = "user_auth")
+    private String userAuth;
 }
