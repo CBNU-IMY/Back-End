df244ccbd288ff0f09496b7ef32f990fd9ca7522
the-groot <sangwoo0795@naver.com>
2022-10-12 21:21:43 +0900

    실행오류버그 fix

--- IMY_server/src/main/java/com/example/imy_server/Domain/User.java
+++ IMY_server/src/main/java/com/example/imy_server/Domain/User.java
@@ -12,14 +12,10 @@ public class User {
 
     @Id @GeneratedValue
     private Long user_pk;
-
     private String user_id;
-
     private String user_pw;
-
     private String user_name;
     private String user_phone;
-
     private String user_auth;
 
 }
